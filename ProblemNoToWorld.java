9cddbdd23d720e243dc9e99deaf9eeb2e4f4be0e
Diksha Trivedi <diksha.trivedi2122@gmail.com>
2025-01-18 15:09:20 +0530

    pushing practice problem

---
+++ ProblemNoToWorld.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class ProblemNoToWorld{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+                switch(n){
+     		case 1:
+		System.out.println("One");
+		break;
+		 case 2:
+                System.out.println("two");
+                break;
+		 case 3:
+                System.out.println("three");
+                break;
+		 case 4:
+                System.out.println("four");
+                break;
+		 case 5:
+                System.out.println("five");
+                break;
+		 case 6:
+                System.out.println("six");
+                break;
+		 case 7:
+                System.out.println("Seven");
+                break;
+		 case 8:
+                System.out.println("Eight");
+                break;
+		 case 9:
+                System.out.println("Nine");
+                break;
+		 case 10:
+                System.out.println("Ten");
+                }
+}
+}
+
